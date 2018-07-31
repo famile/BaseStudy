99a753a80f9885b8a8811bdb53ec114b00f14fa2
litao <437534725@qq.com>
2018-07-31 09:35:20 +0800

    init

---
+++ BaseStudy/BaseStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseStudy
+//
+//  Created by 李涛 on 2018/7/31.
+//  Copyright © 2018年 Tao_Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaseStudy/BaseStudy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BaseStudy
+//
+//  Created by 李涛 on 2018/7/31.
+//  Copyright © 2018年 Tao_Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
